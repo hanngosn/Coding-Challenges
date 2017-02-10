8acf4b03976f2d3fe8833c1d096229e9d0b6ddee
user <Hannie@cu-cs-vm17>
2017-02-10 00:58:26 -0700

    Requirement added

--- TreeRecursion.java
+++ TreeRecursion.java
@@ -1,6 +1,15 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/* Calculate the height of tree after some circulations. 
+Default height of the tree is 1. After the first season, 
+the height is doubled. After the second season, the height is 
+growing 1 more unit. 
+Input format includes <1> the number of test samples
+		      <2> test sample 1
+		      <3> tets sample 2
+*/
+
 public class Tree {
 	
 	
